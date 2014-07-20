6576b5706fff73cb3106a06b41c2509805bbc574
soroosh <soroosh.sarabadani@gmail.com>
2014-07-20 16:17:07 +0430

    detail first version created

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from web_ui.views import HomeView
+from web_ui.views import HomeView, DetailView
 
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^detail/(?P<news_id>\d*)$', DetailView.as_view(), name='detail'),
                        url(r'^(?i)$', HomeView.as_view(), name='home'),
 
 )
--- web_ui/views.py
+++ web_ui/views.py
@@ -16,15 +16,20 @@ class HomeView(View):
         # n.save()
         #
         # while True:
-        #     sleep(1)
+        # sleep(1)
         #
-        #     new_feeds = parser.collect_news_after(date=None)
-        #     if new_feeds is None:
+        # new_feeds = parser.collect_news_after(date=None)
+        # if new_feeds is None:
         #         print 0
         #     else:
         #         print(len(new_feeds))
 
-
-
         return render_to_response('home.html', {'news': News.objects.all()[:10]},
+                                  context_instance=RequestContext(request))
+
+
+class DetailView(View):
+    def get(self, request, news_id):
+        news = News.objects.get(id=news_id)
+        return render_to_response('detail.html', {'news': news},
                                   context_instance=RequestContext(request))
