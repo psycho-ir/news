f7500ab37c36a5af956622c9331fe9e434572cfb
soroosh <soroosh.sarabadani@gmail.com>
2014-07-20 12:31:03 +0430

    unicode method overrided

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class NewsAgency(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     local_name = models.CharField(max_length=200)
+    img_address = models.CharField(max_length=200)
 
     def __unicode__(self):
         return self.name
@@ -24,4 +25,7 @@ class News(models.Model):
     date = models.DateTimeField()
     category = models.ForeignKey(NewsCategory)
     agency = models.ForeignKey(NewsAgency)
+
+    def __unicode__(self):
+        return self.title
 
