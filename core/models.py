edc61a5b2ba9445cc24064c315369e81747e7fce
soroosh <soroosh.sarabadani@gmail.com>
2014-07-20 11:24:59 +0430

    models changed

--- core/models.py
+++ core/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
 
+class NewsAgency(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    local_name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class NewsCategory(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    local_name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
 class News(models.Model):
     title = models.CharField(max_length=200)
     abstract = models.CharField(max_length=500)
     link = models.CharField(max_length=300)
     date = models.DateTimeField()
+    category = models.ForeignKey(NewsCategory)
+    agency = models.CharField(NewsAgency)
 
