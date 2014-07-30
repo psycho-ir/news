1b4e12f2c236f70e529477a5be57737be183332a
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-07-30 14:50:53 +0430

    mptt dependencies removed

--- comments/models.py
+++ comments/models.py
@@ -3,25 +3,3 @@ from django.db import models
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
-
-class MPTTComment(MPTTModel, Comment):
-
-    """ Threaded comments - Add support for the parent comment store and MPTT traversal"""
-
-    # a link to comment that is being replied, if one exists
-
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
-
-
-
-    class MPTTMeta:
-
-        # comments on one level will be ordered by date of creation
-
-        order_insertion_by=['submit_date']
-
-
-
-    class Meta:
-
-        ordering=['tree_id','lft']
