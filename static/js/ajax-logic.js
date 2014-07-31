b2e7b855aa9f8c5f602ea3937d3f15255869cb2d
Vahid Mahdiun <mr_vm_Star@yahoo.com>
2014-07-31 18:25:07 +0430

    filter news by agency with ajax method

---
+++ static/js/ajax-logic.js
@@ -0,0 +1,29 @@
+window.domain = "http://localhost:8000/rest/latest/"
+$(document).ready(function(){
+
+    $(".agency-menu .agency-li").on("click",function(){
+        agencyID = $(this).data("id");
+        url      = window.domain ;
+        $.get(url,{
+            page_number : 1 ,
+            agency      : agencyID
+        },function(data){
+            news     = JSON.parse(data);
+            htmlNews = createNews(news);
+            setTemplate(htmlNews);
+        })
+
+    });
+
+    function createNews(news){
+            var source       = $("#handlebar-news-box").html();
+            var template     = Handlebars.compile(source);
+            var html         = template({news : news});
+            return html;
+    }
+
+    function setTemplate(htmlNews){
+         $(".agency-box").html(htmlNews);
+    }
+
+});
