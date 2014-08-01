5fe68a3a3d2e743f12689573f30d6e3aa6eb4ad6
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-08-01 19:21:05 +0430

    price reader first spike created

---
+++ sample/price.py
@@ -0,0 +1,18 @@
+import urllib
+import json
+
+__author__ = 'SOROOSH'
+
+
+class PriceCrawler:
+    def read_gold_price(self):
+        url = urllib.urlopen('http://www.tala.ir/webservice/price_live.php?mode=ajax&nocache=1')
+        response = url.readline()
+        obj = json.loads(response)
+        return obj
+
+
+c = PriceCrawler()
+result = c.read_gold_price()
+for a in result:
+    print "%s : %s" % (a,result[a])
