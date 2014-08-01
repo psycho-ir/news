b3722704cc69a8e41ee454c313647b19f3b7aa69
Vahid Mahdiun <mr_vm_Star@yahoo.com>
2014-08-01 14:08:11 +0430

    add scroll loading for news

--- static/js/ajax-logic.js
+++ static/js/ajax-logic.js
@@ -1,19 +1,48 @@
 window.domain = "http://localhost:8000/rest/latest/"
+window.pageNumber = 0 ;
+window.agencyId   = 0 ;
 $(document).ready(function(){
 
     $(".agency-menu .agency-li").on("click",function(){
-        agencyID = $(this).data("id");
+        window.pageNumber = 1 ;
+        window.agencyId   = $(this).data("id");
+        callServer(window.pageNumber,agencyId,"refresh")
+    });
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() == $(document).height() - $(window).height()) {
+            window.pageNumber+= 1;
+            if (window.agencyId == 0){
+                callServer(window.pageNumber,0,"append");
+            }
+            else {
+                callServer(window.pageNumber,window.agencyId,"append");
+            }
+        }
+    });
+
+    function callServer(pageNumber,agencyId,type){
         url      = window.domain ;
-        $.get(url,{
-            page_number : 1 ,
-            agencies      : agencyID
-        },function(data){
+        var sendingObject = null;
+        if (agencyId == 0){
+            sendingObject = {
+               page_number : pageNumber
+            }
+        }else {
+            sendingObject = {
+               page_number : pageNumber ,
+               agencies      : agencyId
+            }
+        }
+        $.get(url,sendingObject,function(data){
             news     = JSON.parse(data);
             htmlNews = createNews(news);
-            setTemplate(htmlNews);
+            if (type == "refresh")
+                setTemplate(htmlNews);
+            else
+                appendTemplate(htmlNews);
         })
-
-    });
+    }
 
     function createNews(news){
             var source       = $("#handlebar-news-box").html();
@@ -25,5 +54,10 @@ $(document).ready(function(){
     function setTemplate(htmlNews){
          $(".agency-box").html(htmlNews);
     }
+    function appendTemplate(){
+         $(".agency-box").append(htmlNews);
+    }
+
+
 
 });
