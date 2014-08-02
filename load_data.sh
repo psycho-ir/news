757da9997da5ffd41097e0498dcc20dfea3f850d
soroosh <soroosh.sarabadani@gmail.com>
2014-08-02 17:07:45 +0430

    items.json added to project

--- load_data.sh
+++ load_data.sh
@@ -1,1 +1,1 @@
-python2.7 manage.py loaddata categories.json irna_rss_links.json isna_rss_links.json tabnak_rss_links.json yjc_rss_links.json
+python2.7 manage.py loaddata categories.json irna_rss_links.json isna_rss_links.json tabnak_rss_links.json yjc_rss_links.json items.json
