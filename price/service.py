30e48a715516f71df67ec62274c1a0fd924ced82
soroosh <soroosh.sarabadani@gmail.com>
2014-08-02 16:07:44 +0430

    price service created

--- price/service.py
+++ price/service.py
@@ -4,7 +4,6 @@ import json
 __author__ = 'SOROOSH'
 
 
-
 class PriceCrawler:
     def read_gold_price(self):
         url = urllib.urlopen('http://www.tala.ir/webservice/price_live.php?mode=ajax&nocache=1')
@@ -13,7 +12,8 @@ class PriceCrawler:
         return obj
 
 
-c = PriceCrawler()
-result = c.read_gold_price()
-for a in result:
-    print "%s : %s" % (a,result
+if __name__ == '__main__':
+    c = PriceCrawler()
+    result = c.read_gold_price()
+    for a in result:
+        print "%s : %s" % (a, result[a])
