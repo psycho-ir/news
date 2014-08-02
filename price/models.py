3943d9e022c8918f1850116e3ddc3c47919a0f58
soroosh <soroosh.sarabadani@gmail.com>
2014-08-02 17:14:00 +0430

    pricate items added

--- price/models.py
+++ price/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,primary_key=True)
     local_name = models.CharField(max_length=500)
 
 
