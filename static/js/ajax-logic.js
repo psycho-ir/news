fc9d001187e5f6a3b8f3b20142450df4ead327f8
Vahid Mahdiun <mr_vm_Star@yahoo.com>
2014-08-07 01:16:42 +0430

    add ajax loading in first place

--- static/js/ajax-logic.js
+++ static/js/ajax-logic.js
@@ -1,24 +1,14 @@
-window.apiDomain = "/rest/"
-window.pageNumber = 0 ;
-window.agencyId   = 0 ;
+window.apiDomain   = "/rest/"
+window.pageNumber  = 0 ;
+window.agencyId    = 0 ;
+window.priceAPIUrl = "http://www.tala.ir/webservice/price_live.php?mode=ajax&nocache=1";
 $(document).ready(function(){
 
+    loadLastNews();
     $(".agency-menu .agency-li").on("click",function(){
         window.pageNumber = 1 ;
         window.agencyId   = $(this).data("id");
         callServerForNews(window.pageNumber,agencyId,"refresh")
-    });
-
-    $(".like-news").on("click",function(){
-        var newsId = $(this).data("id");
-        var token  = $(this).data("token");
-        callServerForLike(newsId,token);
-    });
-
-    $(".bookmark-news").on("click",function(){
-        var newsId = $(this).data("id");
-        var token  = $(this).data("token");
-        callServerForBookmark(newsId,token);
     });
 
     $(window).scroll(function() {
@@ -32,29 +22,34 @@ $(document).ready(function(){
             }
         }
     });
-
-    function callServerForBookmark(newsId,token){
+    function callServerForBookmark(newsId,token,object){
         var url           = window.apiDomain + 'bookmark/';
         var sendingObject = {
             news_id : newsId ,
             csrfmiddlewaretoken : token
         };
         $.post(url,sendingObject,function(data){
-
+            data = JSON.parse(data);
+            if (data.message = 'Bookmark saved')
+               $(object).toggleClass('btn-inverse btn-default');
+            else
+               $(object).toggleClass('btn-default btn-inverse');
         })
     }
-
-    function callServerForLike(newsId,token){
+    function callServerForLike(newsId,token,object){
         var url           = window.apiDomain + 'like/';
         var sendingObject = {
             news_id : newsId ,
             csrfmiddlewaretoken : token
         };
         $.post(url,sendingObject,function(data){
-
+            data = JSON.parse(data);
+            if (data.message = 'Like removed')
+               $(object).toggleClass('btn-inverse btn-default');
+            else
+               $(object).toggleClass('btn-default btn-inverse');
         })
     }
-
     function callServerForNews(pageNumber,agencyId,type){
         var url = window.apiDomain + 'latest/' ;
         var sendingObject = null;
@@ -71,30 +66,48 @@ $(document).ready(function(){
         $.get(url,sendingObject,function(data){
             if (data == "{}") return false;
             news     = JSON.parse(data);
+            console.log(news);
             htmlNews = createNews(news);
             if (type == "refresh")
                 setTemplate(htmlNews);
-            else
-                appendTemplate(htmlNews);
+            else{
+                 appendTemplate(htmlNews);
+            }
+
+
+
         })
     }
-
     function createNews(news){
             var source       = $("#handlebar-news-box").html();
             var template     = Handlebars.compile(source);
             var html         = template({news : news });
             return html;
     }
-
     function setTemplate(htmlNews){
          $(".agency-box").html(htmlNews);
+         attachHandler();
     }
     function appendTemplate(){
          $(".agency-box").append(htmlNews);
+         attachHandler();
+    }
+    function loadLastNews(){
+        callServerForNews(1,0,"refresh");
     }
 
-    function likeNews(){
+    function attachHandler(){
+        var token = $("#csrfmiddlewaretoken").val();
+        $(".not-like-bind").on("click",function(){
+            var newsId = $(this).data("id");
+            callServerForLike(newsId,token,this);
 
+        }).toggleClass('not-like-bind is-bind');
+
+        $(".not-bookmark-bind").on("click",function(){
+            var newsId = $(this).data("id");
+            callServerForBookmark(newsId,token,this);
+        }).toggleClass('not-bookmark-bind is-bind');
     }
 
 });
