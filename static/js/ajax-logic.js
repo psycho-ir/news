b365dd33320994a47e926ee3c7eb2fb2dca38102
Vahid Mahdiun <mr_vm_Star@yahoo.com>
2014-08-22 17:18:03 +0430

    add category functionality to project

--- static/js/ajax-logic.js
+++ static/js/ajax-logic.js
@@ -1,33 +1,52 @@
 window.apiDomain            = "/rest/";
 window.pageNumber           = 0 ;
 window.bookmarkPageNumber   = 0 ;
+window.categoryPageNumber   = 0 ;
 window.agencyId             = 0 ;
+window.categoryId           = 0 ;
 window.activeTab            = "#tab-1";
+window.activeRole           = "last-news"; // last-news - category - agency
 $(document).ready(function(){
 
     loadLastNews();
     loadLastPrice();
     loadLastBookmark();
+    loadCategories();
     $(".agency-menu .agency-li").on("click",function(){
         window.pageNumber = 1 ;
+        window.activeRole = 'agency';
         window.agencyId   = $(this).data("id");
         callServerForNews(window.pageNumber,agencyId,"refresh")
     });
+
     $(".tab-list li").on("click",function(){
         var a = $(this).find("a");
         window.activeTab = a.attr("href");
     })
     $(window).scroll(function() {
         if($(window).scrollTop() == $(document).height() - $(window).height()) {
-            var activeTab = window.activeTab;
+            var activeTab  = window.activeTab;
+            var activeRole = window.activeRole;
             if( activeTab == '#tab-1'){
-                window.pageNumber+= 1;
-                if (window.agencyId == 0){
+                if(activeRole == "last-news"){
+                    window.pageNumber+= 1;
+                    callServerForNews(window.pageNumber,0,"append");
+                }
+                else if ( activeRole == "category" ){
+                    window.categoryPageNumber+= 1;
+                    callServerForCategoryNews(window.categoryPageNumber,window.categoryId,"append");
+                }
+                else {
+                    window.pageNumber+= 1;
+                    callServerForNews(window.pageNumber,window.agencyId,"append");
+                }
+
+                /*if (window.agencyId == 0){
                     callServerForNews(window.pageNumber,0,"append");
                 }
                 else {
                     callServerForNews(window.pageNumber,window.agencyId,"append");
-                }
+                }*/
             }
             else if ( activeTab == '#tab-4' ){
                 window.bookmarkPageNumber+= 1;
@@ -50,7 +69,6 @@ $(document).ready(function(){
                $(object).toggleClass('btn-default btn-inverse');
         })
     }
-
     function callServerForBookmarkList(pageNumber){
         var url           = window.apiDomain + 'list_bookmark/';
         sendingObject = {
@@ -108,12 +126,44 @@ $(document).ready(function(){
         var url = window.apiDomain + 'price/' ;
         var sendingObject = null;
         $.get(url,sendingObject,function(data){
-            if (data == "{}") return false;
+            if (data == "{}" || data =="[]") return false;
             var price = JSON.parse(data);
             price.forEach(function(object,b){
                 $("#"+object.item).text(numberWithCommas(object.price));
             })
 
+        })
+    }
+    function callServerForCategory(){
+        var url = window.apiDomain + 'all_categories/' ;
+        var sendingObject = null;
+        $.get(url,sendingObject,function(data){
+            if (data == "{}" || data =="[]") return false;
+            var html = "";
+            data.forEach(function(object,b){
+                var li = $("<li></li>").attr('id',object.pk).text(object.fields.local_name);
+                $(".category-menu").append(li);
+            })
+            setHandlerForCategory();
+        })
+    }
+    function callServerForCategoryNews(pageNumber,categoryId,type){
+        var url = window.apiDomain + 'latest/' ;
+        var sendingObject = null;
+
+            sendingObject = {
+               page_number : pageNumber ,
+               categories      : categoryId
+            }
+
+        $.get(url,sendingObject,function(data){
+            if ( data == "{}" || data == "[]") return false;
+            news     = JSON.parse(data);
+            htmlNews = createNews(news);
+            if (type == "refresh")
+                setTemplate(htmlNews,'.agency-box');
+            else
+                appendTemplate(htmlNews,'.agency-box');
         })
     }
     function createNews(news){
@@ -150,13 +200,23 @@ $(document).ready(function(){
     function loadLastPrice(){
         callServerForPrice();
     }
-
     function loadLastBookmark(){
         window.bookmarkPageNumber = 1 ;
         callServerForBookmarkList(window.bookmarkPageNumber);
     }
-
+    function loadCategories(){
+        callServerForCategory();
+    }
+    function setHandlerForCategory(){
+        $(".category-menu li").on("click",function(){
+            window.categoryPageNumber = 1 ;
+            window.activeRole = 'category';
+            window.categoryId   = $(this).attr("id");
+            callServerForCategoryNews(window.categoryPageNumber,window.categoryId,"refresh")
+        });
+    }
     function numberWithCommas(number) {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
+
 });
