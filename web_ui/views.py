b6c15fa77e0ed6d8f1aeb554a477c4c2cb062519
soroosh <soroosh.sarabadani@gmail.com>
2014-08-28 15:25:53 +0430

    likes and liked added to detail

--- web_ui/views.py
+++ web_ui/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response
 
 from core.models import News
 
+
 class HomeView(View):
     def get(self, request):
         return render_to_response('home.html', {'news': News.objects.all()[0:10]},
@@ -20,6 +21,11 @@ class LoginView(View):
 class DetailView(View):
     def get(self, request, news_id):
         news = News.objects.get(id=news_id)
+        liked = False
+        if request.user.is_authenticated():
+            if news.like_set.filter(user__id=request.user.id):
+                liked = True
+        news.liked = liked
         return render_to_response('detail.html', {'news': news},
                                   context_instance=RequestContext(request))
 
