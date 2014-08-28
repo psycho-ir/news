f96c7489ebbdac490fe878a57e4529d3de0fd422
soroosh <soroosh.sarabadani@gmail.com>
2014-08-28 10:59:37 +0430

    link max_length reduced for production

--- core/models.py
+++ core/models.py
@@ -33,7 +33,7 @@ class News(models.Model):
 
     title = models.CharField(max_length=200)
     abstract = models.CharField(max_length=500)
-    link = models.CharField(max_length=300, unique=True)
+    link = models.CharField(max_length=255, unique=True)
     date = models.DateTimeField()
     category = models.ForeignKey(NewsCategory)
     agency = models.ForeignKey(NewsAgency)
