c511ebc5e0e4801f9818baaf9c5d83f475ec069e
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-08-31 21:33:37 +0430

    debug points added

--- core/crawler/tabnak.py
+++ core/crawler/tabnak.py
@@ -23,12 +23,17 @@ class TabnakCrawler(Crawler):
         cat = None
         try:
             body = soup.find_all('div', {'class': 'body'})[0]
+            print 0
             try:
+                print 1
                 news_path_links = soup.find_all('div', {'class', 'news_path'})[0].find_all('a')
+                print 2
                 for n in news_path_links:
+                    print 3
                     m = re.search(r".*cat_id=(.*).*", n['href'], re.M | re.I)
 
                     if m:
+                        print 4
                         try:
                             cat = cat_ids[m.group(1)]
                         except Exception as e:
