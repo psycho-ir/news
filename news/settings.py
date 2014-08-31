00c749d2111054c83824aa8bb7bc008c2ad63fce
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-08-31 22:36:33 +0430

    static_root commented

--- news/settings.py
+++ news/settings.py
@@ -105,7 +105,7 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_FINDERS = (
