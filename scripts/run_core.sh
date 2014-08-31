e526e9bfa549274d8e28ec22fca019847a08bee3
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-08-31 22:17:41 +0430

    i removed encode utf-8 from url

--- scripts/run_core.sh
+++ scripts/run_core.sh
@@ -1,2 +1,4 @@
 cd ..
-gunicorn -p core_pid  -b localhost:9000 -D  news.production_core_wsgi
+export MYSQL_USER=root
+export MYSQL_PASSWORD=123456
+gunicorn -p core_pid  -b localhost:9000 --error-logfile=log/gunicorn_core.log -D  news.production_core_wsgi
--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -1,2 +1,4 @@
 cd ..
-gunicorn -w 3 -p web_pid -b localhost:8000 -D  news.production_wsgi
+export MYSQL_USER=root
+export MYSQL_PASSWORD=123456
+gunicorn -w 3 -p web_pid --error-logfile=log/gunicorn_web.log -b localhost:8000 -D  news.production_wsgi
