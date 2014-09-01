c2b4bba8bf3820eca706777ec5cf39f37e6ce5dd
soroosh <soroosh.sarabadani@gmail.com>
2014-09-01 10:07:43 +0430

    setup.sh improved

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
 export DJANGO_SETTINGS_MODULE=news.production_settings
+export MYSQL_USER=root
+export MYSQL_PASSWORD=123456
 python2.7 ../manage.py syncdb
 python2.7 ../manage.py migrate
 python2.7 ../manage.py collectstatic
